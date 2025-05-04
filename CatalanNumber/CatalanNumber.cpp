e02b691d245eb77638a159732b5d073ed48823e7
Administrator <Administrator@WIN-20250417HIG>
2025-05-04 12:41:23 +0800

    添加项目文件。

---
+++ CatalanNumber/CatalanNumber.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int n, m, a[30] ;
+int static dfs(int d) {
+	if (d == m + 1) {
+		for (int i = 1; i <= m; i++) {
+			cout << a[i] << " ";
+		}
+		cout << endl;
+		return 1;
+	}
+	for (int i = a[d - 1] + 1; i <= n;i++) {
+		a[d] = i;
+		dfs(d + 1);
+	}
+};
+
+int main() {
+	cin >> n >> m;
+	dfs(1);
+}
